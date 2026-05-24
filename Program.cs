61f3432d38b313edadac3e29bae62f17a1963779
AmolKumbharCoreCard <161825346+askumbhar@users.noreply.github.com>
2026-05-24 12:49:20 +0530

    Updated Policy origin

--- Program.cs
+++ Program.cs
@@ -20,16 +20,13 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        // policy
-        //     .SetIsOriginAllowed(origin => origin.Contains("vercel.app"))
-        //     .AllowAnyHeader()
-        //     .AllowAnyMethod()
-        //     .AllowCredentials();
-
-        policy.AllowAnyHeader()
+        
+        policy.WithOrigins("http://localhost:8081",          // Expo local dev
+            "https://trackpulse-snowy.vercel.app")
+        .AllowAnyHeader()
               .AllowAnyMethod()
-              .AllowCredentials()
-              .SetIsOriginAllowed(_ => true);
+              .AllowCredentials();
+              //.SetIsOriginAllowed(_ => true);
     });
 });
 
@@ -81,27 +78,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast = Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast");
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
 
 
-app.ApplyDbMigrations(connectionString);
+//app.ApplyDbMigrations(connectionString);
 
 app.UseStaticFiles();
 
@@ -121,7 +118,7 @@ app.UseStaticFiles(new StaticFileOptions
 app.MapControllers();
 
 app.Run();
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
